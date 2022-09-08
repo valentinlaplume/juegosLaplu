e5a6c2ce7a85c992f3720f9999f27df079ba3da0
valentinlaplume <laplu.me.valen@gmail.com>
2022-09-08 18:47:46 -0300

    1er sprint - agrego ruteo

--- juegosLaplu/src/app/app-routing.module.ts
+++ juegosLaplu/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
 
+
+const routes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'home', component: HomeComponent},
+  { path:'login', component: LoginComponent},
+  { path:'quienSoy', component: QuienSoyComponent},
+  { path:'**', pathMatch: 'full', redirectTo: 'home'}
+  // { path:'home', component: HomeComponent, children: [
+  // ]},
+  //{ path:'error', component: ErrorComponent},
+  //{ path:'**', component: ErrorComponent}
   
 ];
 
